0b1dd3c6b768f925a22f3c8e391c08925f16bc21
developer <developer@ylabz.com>
2026-08-05 19:41:26 -0700

    new inventory!

--- inventory/runMe.sh
+++ inventory/runMe.sh
@@ -1,5 +1,5 @@
 # Navigate to the specific crate directory
-cd server/kocolor
+cd server/package/KoColor
 
 # Run the binary that compiles the definitions into JSON
 cargo run --bin generate_payload
